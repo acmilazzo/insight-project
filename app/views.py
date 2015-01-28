96d0e595ed9289592be28ebe6d92e703394da9e7
Anna Milazzo <ac.milazzo@gmail.com>
2015-01-27 22:14:42 -0800

    added basic function and pages to app

--- app/views.py
+++ app/views.py
@@ -1,9 +1,15 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Home')
     #return "Hello World"
+    
+@app.route('/return_page', methods=['GET'])
+def return_page():
+    name = request.args.get('name', None)
+    #return_val = {'id1':'somevalue'}
+    return_val = ['SnNa', 'YbSn']
+    return render_template( 'return_page.html', name=name,return_val =return_val)
