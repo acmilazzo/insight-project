861bae9e63e8c7d247e3ef84aee1edd7bc6de6e8
Anna Milazzo <ac.milazzo@gmail.com>
2015-02-02 19:18:45 -0800

    added new files for slides and functionality to nav bar and updated sql

--- app/views.py
+++ app/views.py
@@ -5,13 +5,15 @@ import json
 #read credentialfile
 inobj = open( 'credentials.json', 'r')
 credentials = json.load(inobj)
-db= mdb.connect(user=credentials['user'], password=credentials['password'],host="localhost", db="materials_project", charset='utf8')
+db= mdb.connect(user=credentials['user'], password=credentials['password'],host=credentials['host'], db=credentials['db'], charset='utf8')
 
 @app.route('/')
+def titlepage():
+    return render_template('titlepage.html',title='Home')
+    
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home')
-    #return "Hello World"
+    return render_template('index.html', title='Demo')
     
 @app.route('/return_page', methods=['GET'])
 def return_page():
@@ -19,9 +21,12 @@ def return_page():
     #return_val = {'id1':'somevalue'}
     with db:
         cur= db.cursor()
-        cur.execute("SELECT CASE WHEN material1 = '%s' then material2 else material1 END AS material, value from analysis_results where '%s' in (material1,material2) and type='n_euclid_sprse' order by value asc limit 10" % (name, name))
+        cur.execute( "SELECT CASE WHEN a1.material1 = '%s' then a1.material2 else a1.material1 END AS material, a1.value FROM analysis_results a1 join analysis_results a2 on a1.material1 = a2.material1 and a1.material2 = a2.material2 and a2.type = 'num_feat_sprse' WHERE '%s' in (a1.material1,a1.material2) and a1.type='n_euclid_sprse' and a2.value > 2 ORDER by a1.value asc limit 5" % (name, name))
         query_results = cur.fetchall()
         return_val = []
         for result in query_results:
             return_val.append(dict(material=result[0], n_euclid_sprse=result[1]))
     return render_template( 'return_page.html', name=name,return_val =return_val)
+@app.route('/slides')
+def slides():
+    return render_template('slides.html', title='Slides')
