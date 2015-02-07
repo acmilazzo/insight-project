301e94d2cf74b32da8102a21a84d9aa0a70cb78b
Anna Milazzo <ac.milazzo@gmail.com>
2015-02-07 15:19:29 -0800

    watermark added, new columns to output table

--- app/views.py
+++ app/views.py
@@ -2,13 +2,10 @@ from flask import render_template, request
 from app import app
 import pymysql as mdb
 import json
+
 #read credentialfile
 inobj = open( 'credentials.json', 'r')
 credentials = json.load(inobj)
-#db= mdb.connect(user=credentials['user'], 
-#password=credentials['password'],
-#host=credentials['host'], 
-#db=credentials['db'], charset='utf8')
 
 @app.route('/')
 def titlepage():
@@ -28,26 +25,30 @@ def return_page():
     
     with db:
         cur= db.cursor()
-        
-        cur.execute( """SELECT CASE WHEN a1.material1 = '%s' 
-                          then a1.material2 else a1.material1 END  
-                          AS material, a1.value 
-                        FROM analysis_results a1 
-                          JOIN analysis_results a2 on a1.material1 = a2.material1 
-                            and a1.material2 = a2.material2 
-                            and a2.type = 'num_feat_sprse' 
-                        WHERE '%s' in (a1.material1,a1.material2) 
-                          and a1.type='n_euclid_sprse' and a2.value > 3 
-                        ORDER by a1.value asc limit 5""" % (name, name))
+        cur.execute( """SELECT CASE WHEN material1 = '%s'
+                            THEN material2 ELSE material1 END
+                            AS material, cosine, n_euclid, ktau
+                            FROM analysis
+                            WHERE '%s' in (material1,material2)
+                              and num_feat > 3
+                            ORDER BY n_euclid asc limit 5""" %(name, name))
         
         query_results = cur.fetchall()
         return_val = []
         for result in query_results:
-            sprse_val = round(result[1],3)
-            return_val.append(dict(material=result[0], n_euclid_sprse=sprse_val))
+            sprse_val = round( result[2], 3 )
+            cosine_val = round( result[1], 3 )
+            corr_val = round( result[3], 3 )
+            return_val.append(dict(material=result[0], 
+                              n_euclid_sprse=sprse_val, 
+                              cosine=cosine_val, correlation=corr_val))
     	cur.close()
     db.close()
-    return render_template( 'return_page.html', name=name,return_val =return_val)
+    if len(return_val) >=1 :
+        return render_template( 'return_page.html', name=name,return_val =return_val)
+    else:
+        return render_template( 'return_page.html', name=name)
+
 @app.route('/slides')
 def slides():
     return render_template('slides.html', title='Slides')
